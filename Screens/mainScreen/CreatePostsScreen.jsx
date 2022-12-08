33d5bd318b23b02d4befe075816597507c94d02a
Oleksii Shevchenko <vazerius@gmail.com>
2022-12-08 23:51:08 +0200

    add fonts screen createPost

--- Screens/mainScreen/CreatePostsScreen.jsx
+++ Screens/mainScreen/CreatePostsScreen.jsx
@@ -1,15 +1,36 @@
+import React, { useCallback, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
 import { View, Text } from "react-native";
 import CameraComponent from "../../components/camera";
 import { styles } from "./../styles";
-import { useState } from "react";
+
 import { CreatePost } from "../../components/createPost";
 const CreatePostsScreen = () => {
   const [photo, setPhoto] = useState(null);
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("../../assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("../../assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("../../assets/fonts/Roboto-Bold.ttf"),
+  });
 
+  const onFontsLoaded = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const deletePhoto = () => {
+    setPhoto(null);
+  };
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onFontsLoaded}>
       {photo ? (
-        <CreatePost photo={photo} />
+        <CreatePost photo={photo} deletePhoto={deletePhoto} />
       ) : (
         <CameraComponent setPhoto={setPhoto} />
       )}
