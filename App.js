c99c618da75e6520ceb0ec72e1883361e2a115cb
Oleksii Shevchenko <vazerius@gmail.com>
2022-12-11 20:35:25 +0200

    add redux

--- App.js
+++ App.js
@@ -3,11 +3,17 @@ import React from "react";
 import { useRoute } from "./route";
 
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
 
+import { store } from "./redux/sore";
 const App = () => {
   const routing = useRoute(true);
 
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    <Provider store={store}>
+      <NavigationContainer>{routing}</NavigationContainer>
+    </Provider>
+  );
 };
 
 export default App;
